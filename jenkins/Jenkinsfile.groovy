4fa916baf6566851cb20122f12e7182829c8cc53
Tarun Mehra <tmehra@redhat.com>
2017-12-03 08:03:01 -0800

    fix

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,23 +1,9 @@
 node('maven') {
 
-   stage('Build') {
-         
-       // checkout code from GitHub
-       checkout scm 
-      
-       sh "mvn -s settings.xml clean install -DskipTests"
+   stage('JMeter Test Verification') {
+
    }
-   stage('OpenShift Build') {
-       sh "rm -rf ocp-build && mkdir -p ocp-build/deployments"
-       sh "cp target/*.jar ocp-build/deployments/"
-       sh "oc project tarun-jmeter"
-       sh "oc start-build rules --from-dir=ocp-build --wait=true -n tarun-jmeter"
-   }
-   stage('OpenShift Deployment') {
-       echo "Waiting on OpenShift Deployment..."
-       //openshiftVerifyDeployment depCfg: 'rules', namespace: 'tarun-jmeter', replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '300', waitUnit: 'sec'
-       sleep 100
-   }
+
    stage('JMeter Test Verification') {
        // Finds the route for the rules dev endpoint and overwrites the hostname property in jmeter.proerties with that value.
        // We have to send the output to a file rather than a variable because otherwise we'd have nested backticks.
@@ -27,4 +13,23 @@ node('maven') {
        // Runs JMeter tests and analysis of test results
        sh "mvn -s settings.xml jmeter:jmeter jmeter-analysis:analyze"
    }
+   // Running Performance Plugin with JMeter to show results
+    performanceReport compareBuildPrevious: false,
+        configType: 'ART',
+        errorFailedThreshold: 0,
+        errorUnstableResponseTimeThreshold: '',
+        errorUnstableThreshold: 0,
+        failBuildIfNoResultFile: false,
+        ignoreFailedBuild: false,
+        ignoreUnstableBuild: true,
+        modeOfThreshold: false,
+        modePerformancePerTestCase: true,
+        modeThroughput: true,
+        nthBuildNumber: 0,
+        parsers: [[$class: 'JMeterParser', glob: '20171203-HTTP Request.jtl']],
+        relativeFailedThresholdNegative: 0,
+        relativeFailedThresholdPositive: 0,
+        relativeUnstableThresholdNegative: 0,
+        relativeUnstableThresholdPositive: 0
+
 }
